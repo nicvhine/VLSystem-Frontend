fba93bedd0f72ab931623132d02c66a878b1ea84
Creszra <josephrmagabilin@gmail.com>
2025-06-04 18:27:22 +0800

    fix (landing)
    fixed landing

--- app/components/landing/home.tsx
+++ app/components/landing/home.tsx
@@ -1,12 +1,33 @@
 'use client';
+
+// Import all your section components
 import HeroSection from './heroSection';
 import LandingNavbar from './landingNavbar';
+import AboutUsSection from './aboutussection';
+import Calculator from './calculation'; // Fixed path to match your import
+import FeatureSection from './featuresection';
+import Footer from './footer';
+import LoginModal from './loginModal';
+import Navbar from './navbar';
+import Page from './page';
 
 export default function Home() {
   return (
     <div className="w-full">
-      <LandingNavbar /> 
+      {/* Navigation */}
+      <LandingNavbar />
+      
+      {/* Main Content Sections */}
       <HeroSection />
+      <AboutUsSection />
+      <FeatureSection />
+      <Calculator />
+      
+      {/* Footer */}
+      <Footer />
+      
+      {/* Modal components (these typically render conditionally) */}
+      {/* <LoginModal /> - Usually controlled by state */}
     </div>
   );
 }
