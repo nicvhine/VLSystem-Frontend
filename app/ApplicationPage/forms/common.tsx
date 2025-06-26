8e747164f85596bb8ae3e9dcbb4c9902c9404ce9
nicvhine <21103439@usc.edu.ph>
2025-06-26 17:09:24 +0800

    Fixed marker for map

--- app/ApplicationPage/forms/common.tsx
+++ app/ApplicationPage/forms/common.tsx
@@ -6,6 +6,16 @@ import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
 import axios from "axios";
 
+const customIcon = new L.Icon({
+  iconUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png",
+  shadowSize: [41, 41],
+});
+
+
 
 interface CommonProps {
   appName: string;
@@ -72,9 +82,10 @@ function MapComponent({
   });
 
   return markerPosition ? (
-    <Marker position={markerPosition}>
-      <Popup>{address}</Popup>
-    </Marker>
+  <Marker position={markerPosition} icon={customIcon}>
+    <Popup>{address}</Popup>
+  </Marker>
+
   ) : null;
 }
 
